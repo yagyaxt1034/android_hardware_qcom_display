5afe495574ce0d2b0e70308893acded2e698e331
Yagya Parihar <yagya.parihar@gmail.com>
2020-08-13 13:37:59 -0600

    All is gone except for 8996
    Change-Id: Id147e7ac262527dde95427f9f482507636392fd4

--- Android.mk
+++ Android.mk
@@ -1,21 +1,18 @@
-LOCAL_PATH := $(call my-dir)
-# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
-ifneq ($(TARGET_BOARD_AUTO),true)
-
-ifneq ($(filter msm8x84,$(TARGET_BOARD_PLATFORM)),)
-  include $(call all-named-subdir-makefiles,msm8084)
-else ifneq ($(filter msm8x26,$(TARGET_BOARD_PLATFORM)),)
-  include $(call all-named-subdir-makefiles,msm8226)
-else ifneq ($(filter msm8992,$(TARGET_BOARD_PLATFORM)),)
-  include $(call all-named-subdir-makefiles,msm8994)
-else ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)
-  ifeq ($(TARGET_SUPPORTS_QCOM_3100),true)
-    include $(call all-named-subdir-makefiles,msm8909w_3100)
-  else
-    include $(call all-named-subdir-makefiles,msm8909)
-  endif
-else ifneq ($(wildcard $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)),)
-  include $(call all-named-subdir-makefiles,$(TARGET_BOARD_PLATFORM))
+display-hals := include libcopybit liblight libmemtrack
+ifneq ($(TARGET_USES_GRALLOC1), true)
+    display-hals += libgralloc
+else
+    display-hals += libgralloc1
 endif
 
+display-hals += hdmi_cec
+sdm-libs := sdm/libs
+display-hals += $(sdm-libs)/utils $(sdm-libs)/core $(sdm-libs)/hwc $(sdm-libs)/hwc2
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+    include $(call all-named-subdir-makefiles,$(display-hals))
+else
+ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
+    include $(call all-named-subdir-makefiles,$(display-hals))
+endif
 endif
